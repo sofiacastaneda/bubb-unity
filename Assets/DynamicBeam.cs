2a9ece0ab6ecd9c4cc60e7b918d92fa7c3111584
s/c <47247552+sofiacastaneda@users.noreply.github.com>
2023-02-28 15:20:03 -0500

    a

--- Assets/DynamicBeam.cs
+++ Assets/DynamicBeam.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.MagicLeap;
-
+//using UnityEngine.XR.MagicLeap.MLInput;
 
 public class DynamicBeam : MonoBehaviour
 {
